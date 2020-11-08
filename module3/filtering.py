3afbfea7ce6df89ee30c2254f9786deef66c71ef
André Madeira Cortes <amadeiracortes@gmail.com>
2020-11-08 23:48:50 +0100

    [module3] Add more stop words to filtering.py

--- module3/filtering.py
+++ module3/filtering.py
@@ -9,11 +9,26 @@ except LookupError:
     from nltk.corpus import stopwords
 
 sw = stopwords.words("french")
-sw += ["les", "plus", "cette", "fait", "faire", "être", "deux", "comme", "dont", "tout",
-       "ils", "bien", "sans", "peut", "tous", "après", "ainsi", "donc", "cet", "sous",
-       "celle", "entre", "encore", "toutes", "pendant", "moins", "dire", "cela", "non",
-       "faut", "trois", "aussi", "dit", "avoir", "doit", "contre", "depuis", "autres",
-       "van", "het", "autre", "jusqu"]
+# Stop words added by the teacher
+sw += [
+    "les", "plus", "cette", "fait", "faire", "être", "deux", "comme", "dont",
+    "tout", "ils", "bien", "sans", "peut", "tous", "après", "ainsi", "donc",
+    "cet", "sous", "celle", "entre", "encore", "toutes", "pendant", "moins",
+    "dire", "cela", "non", "faut", "trois", "aussi", "dit", "avoir", "doit",
+    "contre", "depuis", "autres", "van", "het", "autre", "jusqu"
+]
+# My stop words
+sw += [
+    # Avoid over-representation of dates in the word cloud
+    "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août",
+    "septembre", "octobre", "novembre", "décembre", "ans", "années"
+    # Avoid over-representation of non-interesting or obvious words
+    "ville", "bruxelles", "conseil", "communal", "mesdames", "messieurs",
+    "bourgmestre",
+    # Other french stop words which were over-represented
+    "idem", "car", "etc", "quelque", "tant", "telle", "ceux", "elles", "peu",
+    "devant", "voir", "leurs", "lorsque"
+]
 sw = set(sw)
 
 
@@ -24,8 +39,11 @@ def filtering(dpath, year):
     with open(path, encoding='utf-8') as f:
         text = f.read()
         words = nltk.wordpunct_tokenize(text)
-        kept = [w.lower() for w in words if len(
-            w) > 2 and w.isalpha() and w.lower() not in sw]
+        kept = [
+            w.lower()
+            for w in words
+            if len(w) > 2 and w.isalpha() and w.lower() not in sw
+        ]
         kept_string = " ".join(kept)
         output.write(kept_string)
 
