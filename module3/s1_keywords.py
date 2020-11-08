4693f8e1f044d19e6f4e428abbf87dc16a4a721a
André Madeira Cortes <amadeiracortes@gmail.com>
2020-11-08 20:02:09 +0100

    [module3] Modify s1_keywords to add optional arguments
    Extracts by default bigrams or higher for the year 1955.

--- module3/s1_keywords.py
+++ module3/s1_keywords.py
@@ -1,7 +1,23 @@
 """Testing keyword extraction with YAKE"""
 
+import argparse
 import os
+
 import yake
+
+parser = argparse.ArgumentParser(
+    description='Extract keywords from AVB bulletins (in txt form).'
+)
+parser.add_argument(
+    '--year', type=int, default=1955,
+    help='Year to extract from (default: 1955, because we had to chose one)'
+)
+parser.add_argument(
+    '--ngrams', type=int, default=2,
+    help='N-grams or higher to research (default: bigrams or higher)'
+)
+
+args = parser.parse_args()
 
 ignored = set(["conseil communal", "conseil général"])
 
@@ -9,12 +25,12 @@ kw_extractor = yake.KeywordExtractor(lan="fr", top=20)
 data_path = "data/txt/"
 files = os.listdir(data_path)
 for f in sorted(files):
-    if f.startswith("Bxl_"):
+    if f.startswith(f"Bxl_{args.year}"):
         text = open(data_path + f).read()
         keywords = kw_extractor.extract_keywords(text)
         kept = []
         for score, kw in keywords:
             words = kw.split()
-            if len(words) > 1 and kw not in ignored: # only bigrams and more
+            if len(words) > (args.ngrams - 1) and kw not in ignored:  # only bigrams and more
                 kept.append(kw)
         print(f"{f} mentions these keywords: {', '.join(kept)}...")
