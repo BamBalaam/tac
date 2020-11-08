50ee28f5bea2fcbd3f21f32b581f058423b74196
André Madeira Cortes <amadeiracortes@gmail.com>
2020-11-08 21:31:51 +0100

    [module3] Rewrite s2_wordcloud.sh to save files elsewhere
    The files were being saved in a folder that is gitignored.
    
    Also, it makes sense to have the results appear in the context of
    the module being executed.

--- module3/s2_wordcloud.sh
+++ module3/s2_wordcloud.sh
@@ -3,7 +3,9 @@
 # Building a wordcloud based on one year of bulletins
 
 YEAR=$1
-cat data/txt/*_${YEAR}_*.txt > data/${YEAR}.txt
-python module3/filtering.py 'data' $YEAR
-wordcloud_cli --text data/${YEAR}_keywords.txt --imagefile data/${YEAR}.png --width 2000 --height 1000
-display data/${YEAR}.png
+cat data/txt/*_${YEAR}_*.txt > module3/results/${YEAR}.txt
+python module3/filtering.py 'module3/results/' $YEAR
+wordcloud_cli --text module3/results/${YEAR}_keywords.txt \
+              --imagefile module3/results/${YEAR}.png \
+              --width 2000 --height 1000
+display module3/results/${YEAR}.png
